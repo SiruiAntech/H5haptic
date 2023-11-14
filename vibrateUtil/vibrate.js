20fd4322c3df2cbf155563dc5cd802aade0c6da2
sandrivergfw <zjhcyx2003@163.com>
2023-11-14 17:19:55 +0800

    add button for contrast

---
+++ vibrateUtil/vibrate.js
@@ -0,0 +1,32 @@
+var data ={
+    duration:15    
+}
+//解析
+const queryString = window.location.search;
+const queryParams = parseQueryString(queryString)
+const durationValue = queryParams.duration;
+this.data.duration = durationValue;
+vibrate()
+
+function vibrate(){
+    console.log(this.data.duration)
+    navigator.vibrate(this.data.duration);
+}
+function testVibrate(){
+    console.log("对照"+this.data.duration)
+    navigator.vibrate(this.data.duration);
+}
+
+function parseQueryString(queryString) {
+    const params = {};
+    const queryStringWithoutQuestionMark = queryString.substring(1); // 去掉问号
+    const keyValuePairs = queryStringWithoutQuestionMark.split('&');
+  
+    keyValuePairs.forEach(pair => {
+      const [key, value] = pair.split('=');
+      params[key] = decodeURIComponent(value);
+    });
+  
+    return params;
+  }
+  
